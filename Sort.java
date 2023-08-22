48a19e846fb82214d6adcef9dcc73e90f1496686
Julius Klumbies <135857994+MuhahaJulio@users.noreply.github.com>
2023-08-22 18:15:37 +0200

    mika

--- Sort.java
+++ Sort.java
@@ -1,2 +1,23 @@
 public class Sort {
+    private int[] list;
+
+    public Sort (int[] list) {
+        this.list = list;
+    }
+    public void bubbleSort() {
+
+    }
+    public int minimum () {
+
+    }
+    public int maximum () {
+
+    }
+    public void insertionSort() {
+
+    }
+    public void selectionSort() {
+        
+    }
+
 }
