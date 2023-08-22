b7d67f86c0c492ad6e1d57e38dc41f55a54a1980
Julius Klumbies <135857994+MuhahaJulio@users.noreply.github.com>
2023-08-22 19:45:32 +0200

    ztrd

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
         int list[] = {7,4,6,8,5,5,67,7,354,78,7,2,234};
         Sort sort = new Sort(list);
         sort.ausgeben();
-        sort.bubbleSort();
+        sort.selectionSort();
         sort.ausgeben();
     }
 
